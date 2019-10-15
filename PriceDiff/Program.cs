2f25a3a87615a80b7d1214ec796932bc142f71ac
nekto234 <bojchuk2803@gmail.com>
2019-10-15 23:01:47 +0300

    Added csgohouse parser

--- PriceDiff/Program.cs
+++ PriceDiff/Program.cs
@@ -38,14 +38,21 @@ namespace GreenScoreChecker
             chromeOptions.AddArgument(@"--user-data-dir=C:\User\User Data");
             chromeOptions.AddArgument("--profile-directory=Selenium");
             Browser = new ChromeDriver(chromeOptions);
-            List<Item> houseItems = GetHouseItems();
-            List<Diff> difflist = GetSteamItems(houseItems);
-            difflist = difflist.OrderBy(m => m.Percent).ToList();
             string fileName = DateTime.Now.ToString().Replace(".", "+").Replace(":", "+");
+            List<Item> houseItems = GetCSGOHouseItems();
+            List<Diff> difflist = GetSteamItems(houseItems).OrderBy(m => m.Percent).ToList();
             foreach (var item in difflist)
             {
                 Console.WriteLine(item.Name + " " + item.Percent);
-                File.AppendAllText($"./{fileName}.txt", item.Name + " " + item.Percent + " " + item.SteamPrice + " " + item.HousePrice + Environment.NewLine);
+                File.AppendAllText($"./CSGO+{fileName}.txt", item.Name + " " + item.Percent + " " + item.SteamPrice + " " + item.HousePrice + Environment.NewLine);
+            }
+
+            houseItems = GetDOTAHouseItems();
+            difflist = GetSteamItems(houseItems).OrderBy(m => m.Percent).ToList();
+            foreach (var item in difflist)
+            {
+                Console.WriteLine(item.Name + " " + item.Percent);
+                File.AppendAllText($"./DOTA+{fileName}.txt", item.Name + " " + item.Percent + " " + item.SteamPrice + " " + item.HousePrice + Environment.NewLine);
             }
 
         }
@@ -54,17 +61,12 @@ namespace GreenScoreChecker
         {
             List<Diff> list = new List<Diff>();
             HtmlDocument doc = new HtmlDocument();
-            HtmlWeb htmlWeb = new HtmlWeb();
-            GoToUrl("https://steamcommunity.com/market/search?category_570_Hero%5B0%5D=any&category_570_Slot%5B0%5D=any&category_570_Type%5B0%5D=any&appid=570");
-            doc.LoadHtml(Browser.PageSource);
             foreach (var item in houseItems)
             {
-                Browser.FindElement(By.XPath("//*[@id=\"findItemsSearchBox\"]")).SendKeys(item.Name);
-                Browser.FindElement(By.XPath("//*[@id=\"findItemsSearchSubmit\"]")).Click();
-                Thread.Sleep(1000);
+                Thread.Sleep(300);
                 try
                 {
-                    GoToUrl("https://steamcommunity.com/market/search?category_570_Hero%5B%5D=any&category_570_Slot%5B%5D=any&category_570_Type%5B%5D=any&appid=570&q=" + item.Name.Replace(" ", "+"));
+                    GoToUrl("https://steamcommunity.com/market/search?q=" + item.Name.Replace(" ", "+"));
                     doc.LoadHtml(Browser.PageSource);
                     double price = Convert.ToDouble(doc.DocumentNode.SelectSingleNode("//*[@id=\"result_0\"]/div[1]/div[2]/span[1]/span[1]").InnerText.Replace(" pуб.", "").Replace(",", "."), CultureInfo.InvariantCulture);
                     Console.WriteLine(item.Name + " " + item.Price + " " + price);
@@ -73,15 +75,12 @@ namespace GreenScoreChecker
                 {
                     continue;
                 }
-                
-                
             }
             return list;
         }
-        private static List<Item> GetHouseItems()
+        private static List<Item> GetDOTAHouseItems()
         {
             GoToUrl("https://dotahouse.net/shop/");
-            bool check = false;
             List<Item> list = new List<Item>();
             HtmlDocument doc;
             HtmlWeb htmlWeb = new HtmlWeb();
@@ -89,12 +88,10 @@ namespace GreenScoreChecker
             int page = 1;
             while (true)
             {
-
                 var node = doc.DocumentNode.SelectSingleNode("//*[@id=\"withdraw_items\"]");
                 int i = 0;
                 foreach (var item in node.ChildNodes)
                 {
-
                     if (i == node.ChildNodes.Count - 1) break;
                     if (i % 2 == 0)
                     {
@@ -114,6 +111,44 @@ namespace GreenScoreChecker
                 if (node.LastChild.LastChild.GetAttributeValue("class", null).Contains("inactive"))
                     break;
                 doc = htmlWeb.Load("https://dotahouse.net/shop/?page=" + (++page));
+            }
+            return list;
+        }
+
+        private static List<Item> GetCSGOHouseItems()
+        {
+            GoToUrl("https://csgo.house/shop/");
+            List<Item> list = new List<Item>();
+            HtmlDocument doc;
+            HtmlWeb htmlWeb = new HtmlWeb();
+            doc = htmlWeb.Load("https://csgo.house/shop/");
+            int page = 1;
+            while (true)
+            {
+
+                var node = doc.DocumentNode.SelectSingleNode("//*[@id=\"withdraw_items\"]");
+                int i = 0;
+                foreach (var item in node.ChildNodes)
+                {
+                    if (i == node.ChildNodes.Count - 1) break;
+                    if (i % 2 == 0)
+                    {
+                        string name = item.ChildNodes[3].ChildNodes[1].ChildNodes[0].InnerText;
+                        string price = item.ChildNodes[3].ChildNodes[5].ChildNodes[1].GetAttributeValue("data-price", null);
+                        string count = item.ChildNodes[3].ChildNodes[1].ChildNodes[1].ChildNodes[1].InnerText;
+                        list.Add(new Item()
+                        {
+                            Count = Convert.ToInt32(count),
+                            Name = name,
+                            Price = Convert.ToDouble(price, CultureInfo.InvariantCulture)
+                        });
+                        Console.WriteLine(name + " " + count + " " + price);
+                    }
+                    i++;
+                }
+                if (node.LastChild.LastChild.GetAttributeValue("class", null).Contains("inactive"))
+                    break;
+                doc = htmlWeb.Load("https://csgo.house/shop/?page=" + (++page));
             }
             return list;
         }
